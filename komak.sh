be8ed6b368ed9e61ccdec6a634f5596040d2673d
shwan a <81598760+shuvvan@users.noreply.github.com>
2024-11-09 05:28:16 +0330

    Update komak.sh

--- komak.sh
+++ komak.sh
@@ -1,75 +1,68 @@
 #!/bin/bash
 
-clear
-
-# تابع نمایش خوش آمدگویی
+# تابعی برای نمایش پیام خوش آمدگویی
 welcome_message() {
     clear
-    echo -e "\033[0;31m******************************\033[0m"
-    echo -e "\033[1;31m Welcome to Komak Project! \033[0m"
-    echo -e "\033[0;31m******************************\033[0m"
-    echo ""
+    echo -e "\033[0;31m****************************************\033[0m"
+    echo -e "\033[1;31mWel  come to Komak Project!\033[0m"
+    echo -e "\033[0;31m****************************************\033[0m"
+    echo
+    echo "Please select an option:"
+    echo "----------------------------------------"
 }
 
-# تابع نمایش منو
-show_menu() {
-    echo -e "------------------------------"
-    echo -e "1. Update and Upgrade"
-    echo -e "2. Exit"
-    echo -e "------------------------------"
+# نمایش گزینه‌ها
+show_options() {
+    echo "1. Update/Upgrade Server"
+    echo "2. Exit"
+    echo "----------------------------------------"
 }
 
-# تابع اجرای عملیات آپدیت و آپگرید
+# تابع برای آپدیت و آپگریت سرور
 update_upgrade() {
     clear
-    echo "Please wait for update and upgrade your server..."
-    echo ""
-
-    # شروع شمارشگر معکوس
-    SECONDS=0
-
-    # اجرای دستورات آپدیت و آپگرید در پس‌زمینه
-    (sudo apt update && sudo apt upgrade -y) &
-
-    # نمایش شمارشگر
-    while true; do
-        if ! ps aux | grep -q "[a]pt"; then
-            break
-        fi
-        echo -ne "Time elapsed: $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds\r"
+    echo -e "\033[1;33mPlease wait for update and upgrade your server...\033[0m"
+    echo -e "\033[1;33mThis may take a few minutes...\033[0m"
+    echo -e "\033[1;33mProcessing...\033[0m"
+    # شروع عملیات آپدیت و آپگریت در پس‌زمینه
+    sudo apt update && sudo apt upgrade -y &
+    # شناسه پس‌زمینه فرآیند
+    pid=$!
+    # شمارشگر برای مدت زمان
+    seconds=0
+    while kill -0 $pid 2>/dev/null; do
+        ((seconds++))
+        # نمایش شمارشگر به دقیقه و ثانیه
+        minutes=$((seconds / 60))
+        remaining_seconds=$((seconds % 60))
+        echo -e "\033[1;33mTime: $minutes minutes $remaining_seconds seconds\033[0m"
         sleep 1
     done
-
-    # زمانی که عملیات تمام شد
-    echo -e "\nCompleted"
+    echo -e "\033[1;32mUpdate and Upgrade Completed!\033[0m"
     sleep 2
 }
 
-# تابع خروج از اسکریپت
-exit_script() {
-    echo -e "\033[0;31mExiting...\033[0m"
-    sleep 1
-    exit
+# تابع برای نمایش و گرفتن ورودی از کاربر
+get_input() {
+    while true; do
+        welcome_message
+        show_options
+        read -n 1 -s option
+        case $option in
+            1)
+                update_upgrade
+                break
+                ;;
+            2)
+                echo -e "\033[1;31mExiting...\033[0m"
+                break
+                ;;
+            *)
+                echo -e "\033[1;31mInvalid option! Please press 1 to update/upgrade or 2 to exit.\033[0m"
+                ;;
+        esac
+    done
 }
 
-# اجرای پیام خوش آمدگویی
-welcome_message
-
-# نمایش منو و دریافت ورودی
-while true; do
-    show_menu
-    read -p "Please select an option: " option
-
-    case $option in
-        1) 
-            update_upgrade
-            break
-            ;;
-        2)
-            exit_script
-            ;;
-        *)
-            echo "Invalid option, please try again."
-            ;;
-    esac
-done
+# اجرای اسکریپت
+get_input
