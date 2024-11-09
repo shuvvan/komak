91d4b9480f37026969ef76b7e639f8ef1869b2af
shwan a <81598760+shuvvan@users.noreply.github.com>
2024-11-09 07:03:11 +0330

    Update komak.sh

--- komak.sh
+++ komak.sh
@@ -8,7 +8,7 @@ show_welcome_message() {
   BOLD='\033[1m'   # بولد کردن متن
 
   # پیام خوش‌آمدگویی
-  message="Welcome to Komak 2.3 Project!"
+  message="Welcome to Komak 2.3.2 Project!"
   term_width=$(tput cols)  # عرض ترمینال برای وسط‌چین کردن
   message_width=${#message}
   padding=$(( (term_width - message_width - 4) / 2 ))
