10990f12a9f43487c40aafa681aaf529d506ca55
shwan a <81598760+shuvvan@users.noreply.github.com>
2024-12-06 07:03:40 +0330

    Update komak.sh
    3.6.2

--- komak.sh
+++ komak.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-# بررسی دسترسی به sudo و تغییر خودکار به حالت root
-ensure_root_access() {
+# تابع برای بررسی دسترسی و تغییر به روت در پس‌زمینه
+ensure_root_access_in_background() {
   if [ "$(id -u)" -ne 0 ]; then
-    echo -e "\033[0;31mThe script requires root privileges. Switching to root...\033[0m"
-    exec sudo -i bash "$0" "$@"
-    exit 1
+    sudo -v >/dev/null 2>&1 || { echo -e "\033[0;33mSwitching to root...\033[0m"; sudo -i bash "$0" "$@" & }
   fi
 }
 
-# تابع برای بررسی و نصب tput در صورت نیاز
+# تابع برای بررسی و نصب tput در پس‌زمینه
 check_and_install_tput() {
   command -v tput >/dev/null || {
-    echo -e "\033[0;33m'tput' is not installed. Attempting to install...\033[0m"
-    sudo apt update -y && sudo apt install -y ncurses-bin >/dev/null 2>&1
-    if command -v tput >/dev/null; then
-      echo -e "\033[0;32m'tput' installed successfully.\033[0m"
-    else
-      echo -e "\033[0;31mFailed to install 'tput'. Please install it manually and re-run the script.\033[0m"
-      exit 1
-    fi
+    echo -e "\033[0;33m'tput' is not installed. Installing in the background...\033[0m"
+    (sudo apt update -y && sudo apt install -y ncurses-bin >/dev/null 2>&1) &
   }
 }
 
@@ -29,7 +21,7 @@ show_intro_logo() {
   RESET='\033[0m'
   BOLD='\033[1m'
   
-  logo="KOMAK 3.6.1"
+  logo="KOMAK 3.6.2"
   term_width=$(tput cols)
   term_height=$(tput lines)
   logo_width=${#logo}
@@ -52,7 +44,7 @@ show_welcome_message() {
   RESET='\033[0m'
   BOLD='\033[1m'
 
-  message="Welcome to Komak 3.6.1 Project!"
+  message="Welcome to Komak 3.6.2 Project!"
   term_width=$(tput cols)
   message_width=${#message}
   padding=$(( (term_width - message_width - 4) / 2 ))
@@ -169,13 +161,11 @@ show_menu() {
 
 # اجرای اسکریپت
 
-# اجرای بررسی دسترسی روت
-ensure_root_access
-
-# بررسی و نصب tput قبل از اجرای سایر توابع
+# اجرای بررسی دسترسی و نصب tput در پس‌زمینه
+ensure_root_access_in_background
 check_and_install_tput
 
-# نمایش اینترولوگو و ادامه اسکریپت
+# نمایش لوگو و ادامه اسکریپت
 show_intro_logo
 
 while true; do
